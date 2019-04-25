8eac8f977d362e3caff69a61a8040068bf487a89
Radhe Shyam <radhe.shyam@nagarro.com>
2019-04-25 14:24:16 +0530

    updates to deploy to project

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angularfinalproject'));
+app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/angularfinalproject/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
